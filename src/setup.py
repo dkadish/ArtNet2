3a574fe3df68f320cfb5c3d88d5432e1adc9684a
David Kadish <david.kadish@gmail.com>
2020-09-08 09:37:10 +0200

    Setup file

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ArtNet2',
+    version='0.1',
+    packages=['artnet', 'artnet.data', 'artnet.utils'],
+    package_dir={'': 'src'},
+    url='https://github.com/dkadish/ArtNet2',
+    license='GPL-3.0 License',
+    author='David Kadish',
+    author_email='david.kadish@gmail.com',
+    description=''
+)
