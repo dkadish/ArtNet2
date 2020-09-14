88e5029bfd3e08978fdf8ade5c9f2687a08c0ed8
David Kadish <david.kadish@gmail.com>
2020-09-14 16:23:54 +0200

    Updated ArtnetDataset model and added filtered VOC

--- artnet/data/__init__.py
+++ artnet/data/__init__.py
@@ -5,8 +5,7 @@ from typing import Callable, Optional
 import torch
 import torch.utils.data
 from PIL import Image
-
-from torchvision.datasets import VisionDataset
+from torchvision.datasets import VisionDataset, VOCSegmentation, VOCDetection
 
 
 class ArtNetDataset(VisionDataset):
@@ -17,32 +16,46 @@ class ArtNetDataset(VisionDataset):
                  transform: Optional[Callable] = None,
                  target_transform: Optional[Callable] = None) -> None:
         super().__init__(root, transform=transform, transforms=transforms,
-                                    target_transform=target_transform)
+                         target_transform=target_transform)
 
         # load all image files, sorting them to
         # ensure that they are aligned
-        self.imgs = list(sorted((Path(self.root) / "JPEGImages").iterdir()))
-        self.annotations = list(sorted((Path(self.root) / "Annotations").iterdir()))
+        # self.imgs = list(sorted((Path(self.root) / "JPEGImages").iterdir()))
+        # self.annotations = list(sorted((Path(self.root) / "Annotations").iterdir()))
+        self.imgs = list((self._root / 'JPEGImages').glob('**/*.jpg'))
 
     def __getitem__(self, idx):
-        # load images ad masks
-        img_path = self.imgs[idx]
-        annotation_path = self.annotations[idx]
-        img = Image.open(img_path).convert("RGB")
+        # Handle slices
+        if isinstance(idx, slice):
+            start, stop, step = idx.indices(len(self))
+            return [self[i] for i in range(start, stop, step)]
+        elif isinstance(idx, tuple):
+            raise NotImplementedError('Tuple as index')
 
+        # load images and bounding boxes
+        img_path = self.imgs[idx]
+        rel_path = img_path.relative_to(self._root / 'JPEGImages')
+
+        img = Image.open(img_path).convert("RGB")
         boxes = []
-        tree = ET.parse(Path(annotation_path))
-        tree.getroot()
-        root = tree.getroot()
-        objects = root.findall('object')
-        for obj in objects:
-            bndbox = obj.find('bndbox')
-            xmin = int(bndbox.find('xmin').text)
-            ymin = int(bndbox.find('ymin').text)
-            xmax = int(bndbox.find('xmax').text)
-            ymax = int(bndbox.find('ymax').text)
-            bbox = (xmin, ymin, xmax, ymax)
-            boxes.append(bbox)
+
+        try:
+            annotation_path = next((self._root / 'Annotations').glob(str(rel_path.with_suffix('.*'))))
+            # annotation_path = self.annotations[idx]
+            tree = ET.parse(Path(annotation_path))
+            tree.getroot()
+            root = tree.getroot()
+            objects = root.findall('object')
+            for obj in objects:
+                bndbox = obj.find('bndbox')
+                xmin = int(bndbox.find('xmin').text)
+                ymin = int(bndbox.find('ymin').text)
+                xmax = int(bndbox.find('xmax').text)
+                ymax = int(bndbox.find('ymax').text)
+                bbox = (xmin, ymin, xmax, ymax)
+                boxes.append(bbox)
+        except (StopIteration, FileNotFoundError) as e:  # There's no file there.
+            pass
 
         # get bounding box coordinates for each mask
         num_objs = len(boxes)
@@ -52,7 +65,11 @@ class ArtNetDataset(VisionDataset):
         labels = torch.ones((num_objs,), dtype=torch.int64)
 
         image_id = torch.tensor([idx])
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        try:
+            area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        except IndexError as e:
+            area = 0
+
         # suppose all instances are not crowd
         iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
 
@@ -70,7 +87,27 @@ class ArtNetDataset(VisionDataset):
             # print('target: ', target)
             img, target = self.transforms(img, target)
 
-        return img, target#, image_id
+        return img, target  # , image_id
 
     def __len__(self):
         return len(self.imgs)
+
+    @property
+    def _root(self):
+        return Path(self.root)
+
+class VOCDetectionSubset(VOCDetection):
+
+    def __init__(self, root, year='2012', image_set='train', download=False, transform=None, target_transform=None,
+                 transforms=None, names=[]):
+        super().__init__(root, year, image_set, download, transform, target_transform, transforms)
+
+        self.names = names
+
+    def __getitem__(self, index):
+        img, annotation = super().__getitem__(index)
+        annotation['annotation']['object'] = self.filter_annotation_by_object_names(annotation)
+        return img, annotation
+
+    def filter_annotation_by_object_names(self, annotation):
+        return list(filter(lambda o: o['name'] in self.names, annotation['annotation']['object']))
