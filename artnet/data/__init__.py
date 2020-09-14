182a59b52bd8f4039faa9b818fb23c778bc1858b
David Kadish <david.kadish@gmail.com>
2020-09-14 16:31:48 +0200

    Debugging

--- artnet/data/__init__.py
+++ artnet/data/__init__.py
@@ -27,7 +27,8 @@ class ArtNetDataset(VisionDataset):
     def __getitem__(self, idx):
         # Handle slices
         if isinstance(idx, slice):
-            print(idx, len(self))
+            print(idx)
+            print(len(self))
             start, stop, step = idx.indices(len(self))
             return [self[i] for i in range(start, stop, step)]
         elif isinstance(idx, tuple):
