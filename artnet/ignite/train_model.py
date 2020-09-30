cbe8917078c16d65c6cca0b96f7d70f688d001cd
David Kadish <david.kadish@gmail.com>
2020-09-30 10:33:27 +0200

    New box class, no sampler

--- artnet/ignite/train_model.py
+++ artnet/ignite/train_model.py
@@ -63,15 +63,20 @@ class CocoMask(CocoDetection):
 
         return img, new_target
 
+class CocoBox(CocoMask):
+
+    def __init__(self, root, annFile, transform=None, target_transform=None, transforms=None):
+        super().__init__(root, annFile, transform, target_transform, transforms, use_mask=False)
+
 
 def get_data_loaders(train_ann_file, test_ann_file, batch_size, test_size, image_size, use_mask):
     # first, crate PyTorch dataset objects, for the train and validation data.
-    dataset = CocoDetection(
+    dataset = CocoBox(
         root=Path.joinpath(Path(train_ann_file).parent.parent, train_ann_file.split('_')[1].split('.')[0]),
         annFile=train_ann_file,
         transforms=get_transform(train=True, image_size=image_size))#,
         # use_mask=use_mask)
-    dataset_test = CocoDetection(
+    dataset_test = CocoBox(
         root=Path.joinpath(Path(test_ann_file).parent.parent, test_ann_file.split('_')[1].split('.')[0]),
         annFile=test_ann_file,
         transforms=get_transform(train=False, image_size=image_size))#,
@@ -82,16 +87,20 @@ def get_data_loaders(train_ann_file, test_ann_file, batch_size, test_size, image
     indices_val = torch.randperm(len(dataset_test)).tolist()
     dataset_val = torch.utils.data.Subset(dataset_test, indices_val[:test_size])
 
-    print('Training set has {} samples. {} for valication and {} for testing'.format(len(dataset), len(dataset_val), len(dataset_test)))
+    print('Training set has {} samples. {} for validation and {} for testing'.format(len(dataset), len(dataset_val), len(dataset_test)))
 
-    rs = RandomSampler(dataset, replacement=True, num_samples=5000)
-    rs_val = RandomSampler(dataset_val, replacement=True, num_samples=2000)
+    # rs = RandomSampler(dataset, replacement=True, num_samples=5000)
+    # rs_val = RandomSampler(dataset_val, replacement=True, num_samples=2000)
 
     # set train and validation data-loaders
-    train_loader = DataLoader(dataset, batch_size=batch_size, sampler=rs,# shuffle=True,
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True,
                               num_workers=6, collate_fn=safe_collate, pin_memory=True)
-    val_loader = DataLoader(dataset_val, batch_size=batch_size, sampler=rs_val,# shuffle=False,
+    val_loader = DataLoader(dataset_val, batch_size=batch_size, shuffle=False,
                             num_workers=6, collate_fn=safe_collate, pin_memory=True)
+    # train_loader = DataLoader(dataset, batch_size=batch_size, sampler=rs,# shuffle=True,
+    #                           num_workers=6, collate_fn=safe_collate, pin_memory=True)
+    # val_loader = DataLoader(dataset_val, batch_size=batch_size, sampler=rs_val,# shuffle=False,
+    #                         num_workers=6, collate_fn=safe_collate, pin_memory=True)
 
     return train_loader, val_loader, labels_enumeration
 
