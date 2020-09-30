e45079edf8df220ee0e7d135f5664e8fe0df4cc5
David Kadish <david.kadish@gmail.com>
2020-09-30 11:24:06 +0200

    Shrink sample numbers

--- artnet/ignite/train_model.py
+++ artnet/ignite/train_model.py
@@ -89,18 +89,18 @@ def get_data_loaders(train_ann_file, test_ann_file, batch_size, test_size, image
 
     print('Training set has {} samples. {} for validation and {} for testing'.format(len(dataset), len(dataset_val), len(dataset_test)))
 
-    # rs = RandomSampler(dataset, replacement=True, num_samples=5000)
-    # rs_val = RandomSampler(dataset_val, replacement=True, num_samples=2000)
+    rs = RandomSampler(dataset, replacement=True, num_samples=5000)
+    rs_val = RandomSampler(dataset_val, replacement=True, num_samples=2000)
 
     # set train and validation data-loaders
-    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True,
-                              num_workers=6, collate_fn=safe_collate, pin_memory=True)
-    val_loader = DataLoader(dataset_val, batch_size=batch_size, shuffle=False,
-                            num_workers=6, collate_fn=safe_collate, pin_memory=True)
-    # train_loader = DataLoader(dataset, batch_size=batch_size, sampler=rs,# shuffle=True,
+    # train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True,
     #                           num_workers=6, collate_fn=safe_collate, pin_memory=True)
-    # val_loader = DataLoader(dataset_val, batch_size=batch_size, sampler=rs_val,# shuffle=False,
+    # val_loader = DataLoader(dataset_val, batch_size=batch_size, shuffle=False,
     #                         num_workers=6, collate_fn=safe_collate, pin_memory=True)
+    train_loader = DataLoader(dataset, batch_size=batch_size, sampler=rs,# shuffle=True,
+                              num_workers=6, collate_fn=safe_collate, pin_memory=True)
+    val_loader = DataLoader(dataset_val, batch_size=batch_size, sampler=rs_val,# shuffle=False,
+                            num_workers=6, collate_fn=safe_collate, pin_memory=True)
 
     return train_loader, val_loader, labels_enumeration
 
