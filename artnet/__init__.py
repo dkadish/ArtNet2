1dec8bac13ce525601952ef82930ed3197741385
David Kadish <david.kadish@gmail.com>
2020-10-08 11:13:16 +0200

    updated imports

---
+++ artnet/__init__.py
@@ -0,0 +1,4 @@
+import artnet.data
+import artnet.ignite
+import artnet.model
+import artnet.utils
---
+++ artnet/ignite/__init__.py
@@ -0,0 +1,5 @@
+import artnet.ignite.engines
+import artnet.ignite.inference_with_model
+import artnet.ignite.train_model
+import artnet.ignite.transforms
+import artnet.ignite.utilities
