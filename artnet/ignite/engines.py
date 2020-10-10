234a440990bfb675867135e1a2ea841bc11fc668
David Kadish <david.kadish@gmail.com>
2020-10-10 19:02:20 +0200

    Additional debug info

--- artnet/ignite/engines.py
+++ artnet/ignite/engines.py
@@ -13,6 +13,16 @@ def create_trainer(model, device):
         images, targets = copy.deepcopy(batch)
         images_model, targets_model = prepare_batch(batch, device=device)
 
+        print('Images: ')
+        for i, img in enumerate(images_model):
+            print('Image {}: {}'.format(i, type(img)))
+            print('Image {}: {}'.format(i, img.shape))
+
+        print('Targets: ')
+        for i, tgt in enumerate(targets_model):
+            print('Target {}: {}'.format(i, type(tgt)))
+            print('Target {}: {}'.format(i, tgt.shape))
+
         loss_dict = model(images_model, targets_model)
         losses = sum(loss for loss in loss_dict.values())
 
