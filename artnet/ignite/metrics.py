108aaf134c12ba2e76e8e54bc201e0d310081d8d
David Kadish <david.kadish@gmail.com>
2020-11-09 14:30:25 +0100

    Second attempt at a fix

--- artnet/ignite/metrics.py
+++ artnet/ignite/metrics.py
@@ -87,8 +87,9 @@ class CocoMetricBase(Metric):
         return ap, ap5, ap75
 
     def write_tensorboard_pr_curve(self, writer):
-        pr_50, pr_75 = get_pr_levels(self.coco_evaluator.coco_eval)
-        plot_pr_curve_tensorboard(pr_50, pr_75, writer)
+        for res_type in self.coco_evaluator.iou_types:
+            pr_50, pr_75 = get_pr_levels(self.coco_evaluator.coco_eval[res_type])
+            plot_pr_curve_tensorboard(pr_50, pr_75, writer)
 
 class CocoAP(CocoMetricBase):
     def compute(self):
